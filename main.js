7800ad405dc67b56132c47e0e0c180028237f424
Rian <rianssouza02@gmail.com>
2025-06-01 19:44:43 -0300

    Correção media final

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function calculaMediaFinal(){
 function atualizaMediaFinal(){
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = notaMinima;
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
